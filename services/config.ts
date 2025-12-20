9d522bf354991833b4ce5ff70402b4408cce061b
Mohammed-hani69 <zezohani69@gmail.com>
2025-12-20 02:37:13 +0200

    Merge remote GitHub repository with local

--- services/config.ts
+++
@@ -1,6 +0,0 @@
-
-// Set this to FALSE to connect to the local Node.js backend
-// Set this to TRUE to use the mock data (browser-only mode)
-export const USE_MOCK_DATA = true;
-
-export const API_BASE_URL = 'http://localhost:5000/api';
